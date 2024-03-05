11e51d60c445a1de8b601560ee4eb8929051f674
Charles, SPCTR LTD <info@spctr.ltd>
2024-03-05 10:57:40 +0200

    link update

--- link.jsx
+++ link.jsx
@@ -1,10 +1,16 @@
 import Wrapper from './wrapper'
 
 export default props => {
-  const {children, wrapper = false, wrapperClass, ...remainingProps} = props
+  const {children, wrapperChildren, wrapper = false, wrapperClass, ...remainingProps} = props
   return (
     <Wrapper wrap={wrapper} className={wrapperClass}>
-      <a {...remainingProps}>{children}</a>
+      {!Object.prototype.hasOwnProperty.call(remainingProps, 'href') ? (
+        <div {...remainingProps}>{children}</div>
+      ) : undefined}
+      {Object.prototype.hasOwnProperty.call(remainingProps, 'href') ? (
+        <a {...remainingProps}>{children}</a>
+      ) : undefined}
+      {wrapperChildren}
     </Wrapper>
   )
 }
